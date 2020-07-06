ab218345322201e94fdc44e02b025de8b365eded
Eli Caraballo <eli.caraballojr@gmail.com>
2020-07-06 11:55:05 -0500

    Added examples

--- zigZagConversation.js
+++ zigZagConversation.js
@@ -2,6 +2,15 @@
  * @param {string} s
  * @param {number} numRows
  * @return {string}
+ * 
+ *  Input: s = "PAYPALISHIRING", numRows = 4
+    Output: "PINALSIGYAHRPI"
+    Explanation:
+
+    P     I    N
+    A   L S  I G
+    Y A   H R
+    P     I
  */
 
 var convert = function(s, numRows) {
